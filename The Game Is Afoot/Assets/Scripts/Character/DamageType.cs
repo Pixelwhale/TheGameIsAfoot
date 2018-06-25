353d07ba7fc65de8a5ad1549fbfaf6096aa867fe
Pixelwhale <30408629+Pixelwhale@users.noreply.github.com>
2018-06-25 17:26:10 +0900

    CharacterBaseとDamageType作成

---
+++ The Game Is Afoot/Assets/Scripts/Character/DamageType.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AttackType{
+
+    //attack directly
+    physics,
+    fire,
+    cold,
+    lightning,
+    chaos,
+}
+
+public enum DamageOverTimeType{
+
+    bleeding,
+    burning,
+}
